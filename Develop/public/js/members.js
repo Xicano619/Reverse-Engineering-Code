5f3700d0a8f13d6ef7adeaadbc206ba6932c2c80
Xicano619 <ivan.sillasnavarro@gmail.com>
2021-01-15 00:14:57 -0800

    Corrected syntax errors, missing closing semicolon.

--- Develop/public/js/members.js
+++ Develop/public/js/members.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    // This fil;e just does a "GET" request to figure out which user is Logged in.
+    // This fille just does a "GET" request to figure out which user is Logged in.
     // and updates the HTML on the page.
     $.get("/api/user_data").then(function(data) {
         $(".member-name").text(data.email);
